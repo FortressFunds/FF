a16870da33648233c9e9a31968c355622870bb3c
FortressFunds <fundsfortress@gmail.com>
2024-06-01 02:11:57 +0300

    Update and rename roadmap/whitepaper/contract/contract.md to roadmap/whitepaper/contract/contract/token_contract.sol

---
+++ roadmap/whitepaper/contract/contract/token_contract.sol
@@ -0,0 +1,15 @@
+0x34b9b0a1ba026d79e2527a6221950cab6172b62f
+## FortressFunds
+
+Welcome to the official repository for FortressFunds. Here you will find all the key documents and resources related to our project.
+
+### Documents
+
+- [Roadmap](roadmap/roadmap.md)
+- [Whitepaper](whitepaper/whitepaper.md)
+- [Token Contract](contract/token_contract.sol)
+
+### Contract Address
+
+The official contract address for FortressFunds (FF) is:
+0x34b9b0a1ba026d79e2527a6221950cab6172b62f
